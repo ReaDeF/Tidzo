f76aacec808e60215fcd5a45691366f99ac5393a
Mauricio Sánchez <mausadd@outlook.com>
2019-01-26 13:19:31 -0800

    Create service to recover an user password based on the user name.

--- src/common/constant/apiUrl.js
+++ src/common/constant/apiUrl.js
@@ -1,9 +1,12 @@
+import Keys from './keys';
+
 export const securityUrls = {
   login: `${process.env.apiDomain}login`,
 };
 
 export const userUrls = {
-  list: `${process.env.apiDomain}login`,
+  list: `${process.env.apiDomain}list`,
+  recoverPassword: `${process.env.apiDomain}recoverpassword/${Keys.userNameKey}`,
 };
 
 export const foo = {
