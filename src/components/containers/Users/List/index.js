eb3744e56a9feddd2b412660c9c270e4c2fb044d
Carlos Lenon <code@carloslenon.com>
2019-01-31 13:06:33 -0600

    add table component (#36)

--- src/components/containers/Users/List/index.js
+++ src/components/containers/Users/List/index.js
@@ -6,6 +6,7 @@ import { withRouter } from 'react-router-dom';
 // #endregion
 // #region components
 import Toolbar from '../../../presentational/Toolbar';
+import Grid from '../../../presentational/Grid';
 // #endregion
 // #region constant
 // #endregion
@@ -38,8 +39,156 @@ class UsersList extends React.Component {
    * @return {JSX} Components for App
    */
   render() {
+    const columns = [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.id - b.id,
+      },
+      {
+        title: 'Nombres',
+        dataIndex: 'name',
+        key: 'name',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.name - b.name,
+      },
+      {
+        title: 'Apellidos',
+        dataIndex: 'lastName',
+        key: 'lastName',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.lastName - b.lastName,
+      },
+      {
+        title: 'Cargos',
+        dataIndex: 'rol',
+        key: 'rol',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.rol - b.rol,
+      },
+      {
+        title: 'Punto de Venta',
+        dataIndex: 'salesPoint',
+        key: 'salesPoint',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.salesPoint - b.salesPoint,
+      },
+      {
+        title: 'Estado',
+        dataIndex: 'state',
+        key: 'state',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.state - b.state,
+      },
+      {
+        title: 'Fecha de Ingreso',
+        dataIndex: 'dateIn',
+        key: 'dateIn',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.dateIn - b.dateIn,
+      },
+      {
+        title: 'Fecha de Retiro',
+        dataIndex: 'dateOut',
+        key: 'dateOut',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.dateOut - b.dateOut,
+      },
+    ];
+
+    const dataSource = [
+      {
+        key: '1',
+        id: '123456',
+        name: 'Carlos',
+        lastName: 'Lenon',
+        rol: 'React Leader',
+        salesPoint: 'Nicaragua',
+        state: 'Activo',
+        dateIn: '31 Ene. 2019',
+        dateOut: '31 Ene. 2019',
+      },
+      {
+        key: '2',
+        id: '123456',
+        name: 'Carlos',
+        lastName: 'Lenon',
+        rol: 'React Leader',
+        salesPoint: 'Nicaragua',
+        state: 'Activo',
+        dateIn: '31 Ene. 2019',
+        dateOut: '31 Ene. 2019',
+      },
+      {
+        key: '3',
+        id: '123456',
+        name: 'Carlos',
+        lastName: 'Lenon',
+        rol: 'React Leader',
+        salesPoint: 'Nicaragua',
+        state: 'Activo',
+        dateIn: '31 Ene. 2019',
+        dateOut: '31 Ene. 2019',
+      },
+      {
+        key: '4',
+        id: '123456',
+        name: 'Carlos',
+        lastName: 'Lenon',
+        rol: 'React Leader',
+        salesPoint: 'Nicaragua',
+        state: 'Activo',
+        dateIn: '31 Ene. 2019',
+        dateOut: '31 Ene. 2019',
+      },
+      {
+        key: '5',
+        id: '123456',
+        name: 'Carlos',
+        lastName: 'Lenon',
+        rol: 'React Leader',
+        salesPoint: 'Nicaragua',
+        state: 'Activo',
+        dateIn: '31 Ene. 2019',
+        dateOut: '31 Ene. 2019',
+      },
+      {
+        key: '6',
+        id: '123456',
+        name: 'Carlos',
+        lastName: 'Lenon',
+        rol: 'React Leader',
+        salesPoint: 'Nicaragua',
+        state: 'Activo',
+        dateIn: '31 Ene. 2019',
+        dateOut: '31 Ene. 2019',
+      },
+    ];
+
+    // const pagination = {
+    //   hideOnSinglePage: true,
+    // };
+
+    const rowSelection = {
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+      },
+      getCheckboxProps: record => ({
+        disabled: record.name === 'Disabled User', // Column configuration not to be checked
+        name: record.name,
+      }),
+    };
     return (
-      <Toolbar />
+      <>
+        <Toolbar />
+        <Grid
+          columns={columns}
+          data={dataSource}
+          rowSelection={rowSelection}
+        />
+      </>
     );
   }
 }
